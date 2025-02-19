5043c299c2fd1feca7202c0a8db6bcd2614ff1df
Neo <iladorovs@yandex.ru>
2025-02-20 01:40:48 +0600

    chore: created structure and initial settings of project

---
+++ src/app.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+
+export default function App() {
+  const app = express();
+  const prisma = new PrismaClient();
+  app.use(express.json());
+
+  app.post("/bookmarks", async (req, res) => {
+    const { url, title, description } = req.body;
+    const bookmark = await prisma.bookmark.create({
+      data: {
+        url,
+        title,
+        description,
+      },
+    });
+    res.json(bookmark);
+  });
+
+  app.get("/bookmarks", async (req, res) => {
+    const bookmarks = await prisma.bookmark.findMany();
+    res.json(bookmarks);
+  });
+
+  app.listen(3000, () => {
+    console.log("Server is running on http://localhost:3000");
+  });
+}
